037a495adcfed3630897d75a5a91d3efef1bd6f1
verde-mar <sara.grecu25@gmail.com>
2021-10-11 15:35:30 +0200

    Modifiche Client e Server

--- src/main/java/WORTH/server/MainClass.java
+++ src/main/java/WORTH/server/MainClass.java
@@ -19,10 +19,17 @@ public class MainClass {
         }
     }
 
-
+    /**
+     * Se non esiste gia', crea la directory 'projects' dove saranno memorizzatu tutti i progetti.
+     * Poi chiama una funzione ausiliaria per inizializzare i progetti in memoria
+     * @param projects Struttura dati in memoria contenente i progetti degli utenti
+     * @throws Exception Nel caso in cui non possa essere creata la directory dei progetti 'projects'
+     */
     private static void set(ConcurrentHashMap<String, Project> projects) throws Exception {
         File direct = new File("./projects");
+        /* Se la directory non esiste gia' */
         if (!direct.exists()) {
+            /* La directory viene creata */
             boolean worth_dir = direct.mkdir();
             if(!worth_dir)
                 throw new Exception("Couldnt create the directory 'projects'");
@@ -31,6 +38,12 @@ public class MainClass {
         }
     }
 
+    /**
+     * La funzione inizializza i progetti in memoria
+     * @param direct Il file della directory
+     * @param projects Struttura in memoria che conterra' i progetti nel server
+     * @throws Exception Nel caso in cui non possa essere creato un nuovo progetto in memoria
+     */
     private static void setProject(File direct, ConcurrentHashMap<String, Project> projects) throws Exception {
         if (direct.isDirectory()) {
             File[] fil = Objects.requireNonNull(direct).listFiles();
--- src/main/java/WORTH/server/RemoteRegister.java
+++ src/main/java/WORTH/server/RemoteRegister.java
@@ -12,8 +12,13 @@ import java.util.List;
 public class RemoteRegister extends RemoteServer implements RemoteInterface {
     /* Gestore dei metodi associati agli utenti */
     private final UserManager userManager;
+    /* Lista dei client registrati al servizio di notifica */
     private final List<NotifyInterface> clients;
 
+    /**
+     * Costruttore della classe
+     * @throws Exception Nel caso in cui qualcuno abbia gia'
+     */
     protected RemoteRegister() throws Exception {
         super();
         this.userManager = UserManager.getIstance();
@@ -30,6 +35,12 @@ public class RemoteRegister extends RemoteServer implements RemoteInterface {
         userManager.register(nickUtente, password);
     }
 
+    /**
+     * Registra il client al servizio di notifiche RMI
+     * @param clientInterface Client registrato al servizio di notifiche RMI
+     * @return List<User> Lista degli utenti registrati
+     * @throws Exception Nel caso in cui il client sia gia' registrato
+     */
     @Override
     public synchronized List<User> registerForCallback(NotifyInterface clientInterface) throws Exception {
         ArrayList<User> users = new ArrayList<>(userManager.getUtenti().values());
@@ -41,6 +52,11 @@ public class RemoteRegister extends RemoteServer implements RemoteInterface {
         return users;
     }
 
+    /**
+     * De-registra il client al servizio di notifiche RMI
+     * @param clientInterface Client registrato al servizio di notifiche RMI
+     * @throws RemoteException Nel caso in cui ci siano errori nell'interazione RMI
+     */
     @Override
     public synchronized void unregisterForCallback(NotifyInterface clientInterface) throws RemoteException {
         if(clients.remove(clientInterface))
