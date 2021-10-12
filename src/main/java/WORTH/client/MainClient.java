6ea94783cab74456892880d345c3dc23e3a949ef
verde-mar <sara.grecu25@gmail.com>
2021-10-12 09:55:47 +0200

    Modifiche Client e Server

--- src/main/java/WORTH/client/MainClient.java
+++ src/main/java/WORTH/client/MainClient.java
@@ -13,7 +13,6 @@ import java.util.regex.Pattern;
 
 //todo: c'e' da testare tutte le operazioni con molti client e in sezione critica
 //todo: c'e' da non permettere che un utente faccia delle operazioni se non e' loggato
-//todo: penso manchi il metodo help
 public class MainClient extends JFrame {
     private static boolean login = false;
 
@@ -48,7 +47,7 @@ public class MainClient extends JFrame {
                 throw new Exception("Description must be between \"\" ");
             }
             response = worthClient.addCard(nickUtente, projectName, cardName, description);
-        } else if (tokens.get(0).equals("move_card")&& login) {
+        } else if (tokens.get(0).equals("move_card")&& login) { //todo: devi aggiornare la chat
             String nickUtente = tokens.get(1);
             String projectName = tokens.get(2);
             String cardName = tokens.get(3);
@@ -93,6 +92,23 @@ public class MainClient extends JFrame {
             response = worthClient.logout(tokens.get(1));
             rmiClient.unregisterForCallback();
             Thread.currentThread().interrupt();
+        } else if(tokens.get(0).equals("help")){
+            System.out.println("You asked for help. Here's the syntax:\n");
+            System.out.println("command param1 ...");
+            System.out.println("register userName password");
+            System.out.println("login userName password");
+            System.out.println("logout userName");
+            System.out.println("list_users");
+            System.out.println("list_online_users");
+            System.out.println("list_projects userName");
+            System.out.println("create_project userName nameProject");
+            System.out.println("add_card userName nameProject cardName cardDescription");
+            System.out.println("get_card_history nickName nameProject cardName");
+            System.out.println("show_members userName nameProject");
+            System.out.println("add_member userName nameProject userNameToAdd");
+            System.out.println("show_card userName nameProject cardName");
+            System.out.println("cancel_project userName nameProject");
+            System.out.println("move_card userName nameProject cardName DepartureList ArrivalList");
         } else {
             throw new Exception("You have'nt loggedin or this method doesn't exist");
         }
